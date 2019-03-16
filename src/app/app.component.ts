d4418cac58ec7a87aaa7816318c5aff75583912d
Sergei Gavrish <sergei_gavrish@epam.com>
2019-03-16 18:07:06 +0300

    [task-7] title support was added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Title } from "@angular/platform-browser";
+import { ActivatedRoute, Router, NavigationEnd, PRIMARY_OUTLET } from "@angular/router";
+
+import { BehaviorSubject } from "rxjs";
+import { filter, map, switchMap } from "rxjs/operators";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = "angular2018q4";
+export class AppComponent implements OnInit {
+
+  constructor(
+    private title: Title,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.setTitle();
+  }
+
+  private setTitle() {
+    this.router.events
+      .pipe(
+        filter(e => e instanceof NavigationEnd),
+        map(() => this.route),
+        map(route => {
+          let routeWithTitle = route;
+          while (route.firstChild) {
+            if ((<BehaviorSubject<any>>route.firstChild.data).getValue().title) {
+              routeWithTitle = route.firstChild;
+            }
+            route = route.firstChild;
+          }
+          return routeWithTitle;
+        }),
+        filter(route => route.outlet === PRIMARY_OUTLET),
+        switchMap(route => route.data)
+      )
+      .subscribe(data => {
+        this.title.setTitle(data.title);
+      });
+  }
 }
